19e34dcaaa8224f6425ef4787acb8f7b1fb1af6b
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-12-08 17:11:54 +0100

    Loading component installed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,65 @@
-import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
 import {COLORS} from '../theme/theme';
-import {TouchableOpacity} from 'react-native';
+import {
+  upComingMovies,
+  nowPlayingMovies,
+  popularMovies,
+  baseImagePath,
+} from '../api/apicalls';
+
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = ({navigation}: any) => {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.push('MovieDetails');
-        }}>
-        <Text>HomeScreen</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  const [nowPlayingMoviesList, setnowPlayingMoviesList] =
+    useState<any>(undefined);
+  const [upcomingMoviesList, setupComingMoviesList] = useState<any>(undefined);
+  const [popularMoviesList, setpopularMoviesList] = useState<any>(undefined);
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return (
+      <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    );
+  }
+  return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     backgroundColor: COLORS.Black,
+  },
+  //For middle content
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
 });
 
