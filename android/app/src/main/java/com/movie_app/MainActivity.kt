107bf6a83452a934c3d656899dd988834fcd6da3
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-12-08 15:51:43 +0100

    react navigator installed in kotlin

--- android/app/src/main/java/com/movie_app/MainActivity.kt
+++ android/app/src/main/java/com/movie_app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.movie_app
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,12 +7,14 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "Movie_App"
-
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
