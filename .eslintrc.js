7baf228f88a113551f0c185fe08004bab807814c
Batuhan Ayaz <batuhan.ayaz@student.ehb.be>
2023-12-08 14:46:43 +0100

    Eslint positive error fixed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   extends: '@react-native',
   rules: {
     'prettier/prettier': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'none',
+      },
+    ],
   },
 };
